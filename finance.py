9089ea8ecb61b01b94feeccf8c3034cef278ce79
Jeffrey Chen <31812968+jlchen19@users.noreply.github.com>
2018-11-05 22:04:04 -0800

    Add files via upload

--- finance.py
+++ finance.py
@@ -1,33 +1,30 @@
 import requests
-import xlrd
-import time
+import pandas
+import openpyxl
 
 def read_tickers(filename): #filename in xlsx presumably
-    stock_prices = xlrd.open_workbook(filename)
-    sheet = stock_prices.sheet_by_index(0)
-    return sheet.col_values(0,1)
+    sheet = openpyxl.load_workbook(filename).get_sheet_by_name("Sheet1")
+    return [x.value for x in sheet["A"]][1:]
 
+print(read_tickers("listcode.xlsx"))
+
+def add_new_prices(prices, filename, new_file):
+    workbook = openpyxl.load_workbook(filename)
+    sheet = workbook.get_sheet_by_name("Sheet1")
+    columns = sheet.max_column + 1
+    for row, price in enumerate(prices, 1): 
+        sheet.cell(row=row, column=columns, value=price)
+    workbook.save(new_file)            
 url = 'https://www.alphavantage.co/query'
-tickers = read_tickers("listcode.xlsm")
-
-prices = {}
+tickers = ['MSFT','GOOG','X']
 
 for sym in tickers:
-	while True:
-		params = dict(
-			function='GLOBAL_QUOTE',
-			symbol=sym,
-			apikey='XXXXXXXX'
-		)
+	params = dict(
+		function='GLOBAL_QUOTE',
+		symbol=sym,
+		apikey='ZCQPZJ9MFTUI4OX6'
+	)
 
-		resp = requests.get(url=url, params=params)
-		data = resp.json()
-		if("Global Quote" in data):
-			print(str(sym))
-			print(data["Global Quote"]["05. price"])
-			prices[sym] = data["Global Quote"]["05. price"]
-			break
-	
-		
-
-	
+	resp = requests.get(url=url, params=params)
+	data = resp.json()
+	print(data["Global Quote"]["05. price"])
