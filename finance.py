9a9280207d5ad43c94775ea4317f66f89eb234c7
Mehul Raheja <rahejamehul@gmail.com>
2018-11-05 21:26:10 -0800

    read
    Added reading in

--- finance.py
+++ finance.py
@@ -1,26 +1,33 @@
 import requests
-import pandas
 import xlrd
+import time
+
 def read_tickers(filename): #filename in xlsx presumably
     stock_prices = xlrd.open_workbook(filename)
     sheet = stock_prices.sheet_by_index(0)
     return sheet.col_values(0,1)
-print(read_tickers("listcode.xlsm"))
 
-'''
 url = 'https://www.alphavantage.co/query'
-tickers = ['MSFT','GOOG','X']
+tickers = read_tickers("listcode.xlsm")
+
+prices = {}
 
 for sym in tickers:
-	params = dict(
-		function='GLOBAL_QUOTE',
-		symbol=sym,
-		apikey='XXXXXXXXXX'
-	)
+	while True:
+		params = dict(
+			function='GLOBAL_QUOTE',
+			symbol=sym,
+			apikey='XXXXXXXX'
+		)
 
-	resp = requests.get(url=url, params=params)
-	data = resp.json()
-	print(data["Global Quote"]["05. price"])
+		resp = requests.get(url=url, params=params)
+		data = resp.json()
+		if("Global Quote" in data):
+			print(str(sym) + str(keys[U]))
+			print(data["Global Quote"]["05. price"])
+			price[sym] = data["Global Quote"]["05. price"]
+			break
+	
+		
 
 	
-'''
